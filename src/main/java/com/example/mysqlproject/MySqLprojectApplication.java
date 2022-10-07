2c401aeb4232d0b2931b53297227b5eac933bf7e
Ella Frost <ded_19@hotmail.com>
2022-10-07 09:22:25 +0200

    New

---
+++ src/main/java/com/example/mysqlproject/MySqLprojectApplication.java
@@ -0,0 +1,13 @@
+package com.example.mysqlproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MySqLprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MySqLprojectApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/example/mysqlproject/repository/DatabaseConnectionManager.java
@@ -0,0 +1,12 @@
+package com.example.mysqlproject.repository;
+
+import java.sql.Connection;
+
+public class DatabaseConnectionManager {
+
+    //variabler
+    private static String hostname;
+    private static String username;
+    private static String password;
+    private static Connection conn;
+}
